28117baf3c120e4044dbce0c0ec102ecdba1b2a0
michaelckayp <58499828+michaelckayp@users.noreply.github.com>
2023-02-24 10:26:33 -0700

    Created Bert Model and used makeModels to call them

--- makeModels.py
+++ makeModels.py
@@ -1,23 +1,48 @@
-from sklearn.model_selection import train_test_split
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 # Citation:
-# https://www.geeksforgeeks.org/how-to-do-train-test-split-using-sklearn-in-python/
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
 
-# Split Data into train and test to be used for the models
-def splitData(twitter_df):
-    x = twitter_df["Tweet"]
-    y = twitter_df["stance"]
-    return train_test_split(x,y,random_state=0, test_size=0.20, shuffle=True)
-
-# Caleb's Model
-# https://towardsdatascience.com/transfer-learning-in-nlp-for-tweet-stance-classification-8ab014da8dde
-def transferLearning(x_train, x_test, y_train, y_test):
-    pass
+import Bert_Model_Baseline
+import Bert_Model
 
 
-# Main Code to call other functions
-path = "./Cleaned_Climate_Change_Tweets_Model.csv"
-twitter_df = pd.read_csv(path).dropna()
-x_train, x_test, y_train, y_test = splitData(twitter_df)
+def balance_data(df):
+    df_b = df[df["stance"]=='believer']
+    df_d = df[df["stance"]=='denier']    
+    df_balanced = pd.DataFrame()
+    if len(df_b) > len(df_d):
+        df_b_dwnsmpld = df_b.sample(df_d.shape[0])
+        df_balanced = pd.concat([df_b_dwnsmpld, df_d])
+    elif len(df_b) < len(df_d):
+        df_d_dwnsmpld = df_d.sample(df_b.shape[0])
+        df_balanced = pd.concat([df_d_dwnsmpld, df_b])
+    else:
+        return df
+    return df_balanced
+
+def analyze_results(test_labels, prediction_labels):
+    return f1_score(test_labels, prediction_labels, average="binary")
+
+def main():
+    df = pd.read_csv("./Climate_Change_Tweets_Model.csv")
+    df = df[df["stance"] != "neutral"]
+    df = balance_data(df)
+    df["stance_label"] = df["stance"].map({"believer": 1, "denier": 0})
+
+    model_texts = df["Tweet"].to_list()
+    model_labels = df["stance_label"].to_list()
+
+    train_texts, test_texts, train_labels, test_labels = train_test_split(model_texts,model_labels, stratify=model_labels, test_size=.2)
+
+    prediction_labels = Bert_Model_Baseline.roberta_model_baseline(test_texts)
+    score = analyze_results(test_labels, prediction_labels)
+
+    prediction_labels = Bert_Model.robert_model_tf(train_texts, test_texts, train_labels, test_labels)
+    score = analyze_results(test_labels, prediction_labels)
+
+
+
+
